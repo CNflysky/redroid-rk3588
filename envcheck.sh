88d99e74fab91e64b328dcd123888c32562aade2
CNflysky <cnflysky@qq.com>
2024-12-23 18:53:52 +0800

    scripts: remove mali kernel driver version requirements

--- envcheck.sh
+++ envcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export SCRIPT_VER=0.4
+export SCRIPT_VER=0.5
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
@@ -21,41 +21,33 @@ check_kernel_version() {
     then
 	color_echo $GREEN "Kernel version: `uname -r`"
     else
-        color_echo $RED "Kernel version mismatch: `uname -r`"
-        export KERNEL_VERSION_MISMATCH=1
+        color_echo $RED "Kernel version unsupported: `uname -r`"
+        export UNSUPPORTED_KERNEL=1
     fi
 }
 
 check_mali_driver() {
-    if [ -f /sys/module/bifrost_kbase/version ]
+    if $GREP_EXE -qP "^CONFIG_MALI_BIFROST=(y|m)" $CONFIG_PATH > /dev/null 2>&1
     then
-	if grep -q "g18p0" /sys/module/bifrost_kbase/version > /dev/null 2>&1
-	then
-		color_echo $GREEN "Mali DDK verison: `cat /sys/module/bifrost_kbase/version`"
+		color_echo $GREEN `$GREP_EXE -P "^CONFIG_MALI_BIFROST=(y|m)" $CONFIG_PATH`
 	else
-		color_echo $RED "Mali DDK version mismatch: `cat /sys/module/bifrost_kbase/version`"
-		export MALI_DDK_VER_MISMATCH=1
-	fi
-    else
-	color_echo $RED "Mali kernel driver missing."
-	export MALI_KERNEL_DRIVER_MISSING=1
+        color_echo $RED "Mali GPU kernel driver missing."
+	    export NO_MALI_KERNEL_DRIVER=1
     fi
 }
 
 check_mali_firmware(){
 	if [ -f /lib/firmware/mali_csffw.bin ]
 	then
-		mali_fw_git_sha=`strings /lib/firmware/mali_csffw.bin | grep git_sha | cut -d ' ' -f 2`
-		if [ $mali_fw_git_sha == ee476db42870778306fa8d559a605a73f13e455c ]
-		then
-			color_echo $GREEN "Mali CSF Firmware git_sha: $mali_fw_git_sha"	
-		else
-			color_echo $PURPLE "Mali CSF Firmware git_sha mismatch: $mali_fw_git_sha"
-			export MALI_CSF_FW_GIT_SHA_MISMATCH=1
-		fi
+		color_echo $GREEN "Mali CSF Firmware exists."
 	else
-		color_echo $PURPLE "Mali CSF Firmware missing."
-		export MALI_CSF_FW_MISSING=1
+        if $GREP_EXE -q "CONFIG_MALI_CSF_INCLUDE_FW=y" $CONFIG_PATH > /dev/null 2>&1
+        then 
+            color_echo $GREEN "Mali CSF Firmware was built into your kernel."
+        else
+		    color_echo $PURPLE "Mali CSF Firmware missing."
+		    export MALI_CSF_FW_MISSING=1
+        fi
 	fi
 }
 
@@ -76,7 +68,7 @@ check_kernel_features(){
 		color_echo $GREEN "CONFIG_ANDROID_BINDERFS=y"
 	else
 		color_echo $RED "CONFIG_ANDROID_BINDERFS is not enabled in your kernel."
-		export BINDERFS_MISSING=1
+		export NO_BINDERFS=1
 	fi
 
 	if $GREP_EXE -q "CONFIG_PSI=y" $CONFIG_PATH > /dev/null 2>&1
@@ -84,7 +76,7 @@ check_kernel_features(){
 		color_echo $GREEN "CONFIG_PSI=y"
 	else
 		color_echo $RED "CONFIG_PSI is not enabled in your kernel."
-		export PSI_MISSING=1
+		export NO_PSI=1
 	fi
 
 	if $GREP_EXE -q "CONFIG_ARM64_VA_BITS=39" $CONFIG_PATH > /dev/null 2>&1
@@ -92,7 +84,7 @@ check_kernel_features(){
  		color_echo $GREEN "CONFIG_ARM64_VA_BITS=39"
 	else
  		color_echo $PURPLE "CONFIG_ARM64_VA_BITS does not match recommended value."
-		export VA_MISSING=1
+		export VA_MISMATCH=1
 	fi
 }
 
@@ -120,17 +112,22 @@ check_env(){
     color_echo $YELLOW "checking kernel version..."
     check_kernel_version
     color_echo $GREEN "========================================"
-    color_echo $YELLOW "checking mali driver version..."
-    check_mali_driver
-    color_echo $GREEN "========================================"
-    color_echo $YELLOW "checking mali firmware version..."
-    check_mali_firmware
-    color_echo $GREEN "========================================"
     color_echo $YELLOW "checking kernel config location..."
     check_kernel_config_location
+    if [ -z "$CONFIG_PATH" ]
+    then
+        color_echo $RED "FATAL: kernel config missing, aborted"
+        return 1
+    fi
+    color_echo $GREEN "========================================"
+    color_echo $YELLOW "checking mali gpu driver..."
+    check_mali_driver
+    color_echo $GREEN "========================================"
+    color_echo $YELLOW "checking mali firmware..."
+    check_mali_firmware
     color_echo $GREEN "========================================"
     color_echo $YELLOW "checking kernel features..."
-    [ -z "$CONFIG_PATH" ] && color_echo $RED "kernel config missing: skipped." || check_kernel_features
+    check_kernel_features
     color_echo $GREEN "========================================"
     color_echo $YELLOW "checking binderfs..."
     check_binderfs
@@ -144,21 +141,14 @@ print_summary() {
     color_echo $YELLOW Summary
     export password=$(echo -n "$(md5sum "$0" | awk '{print $1}')$(md5sum /dev/null | awk '{print $1}')" | md5sum | awk '{print $1}')
     color_echo $GREEN "QQ channel password: $password"
-    [ -n "$KERNEL_VERSION_MISMATCH" ] && color_echo $RED "FATAL: Kernel version mismatch" && export FATAL=1
-    [ -n "$MALI_KERNEL_DRIVER_MISSING" ] && color_echo $RED "FATAL: Mali kernel driver missing" && export FATAL=1
-    [ -n "$MALI_DDK_VER_MISMATCH" ] && color_echo $RED "FATAL: Mali DDK version mismatch" && export FATAL=1
+    [ -n "$UNSUPPORTED_KERNEL" ] && color_echo $RED "FATAL: Kernel version mismatch" && export FATAL=1
+    [ -n "$NO_MALI_KERNEL_DRIVER" ] && color_echo $RED "FATAL: Mali kernel driver missing" && export FATAL=1
     [ -n "$NO_ANDROID_DMA_BUF_DEVICE" ] && color_echo $RED "FATAL: Android specific dma-buf heap device missing" && export FATAL=1
-    if [ -z "$CONFIG_PATH" ] 
-    then
-        color_echo $RED "ERROR: Can not find your kernel config. Some required kernel feature(s) may not enabled in your kernel."
-    else
-        [ -n "$BINDERFS_MISSING" ] && color_echo $RED "FATAL: CONFIG_ANDROID_BINDERFS is not enabled in your kernel" && export FATAL=1
-        [ -n "$PSI_MISSING" ] && color_echo $RED "FATAL: CONFIG_PSI is not enabled in your kernel" && export FATAL=1
-    fi
+    [ -n "$NO_BINDERFS" ] && color_echo $RED "FATAL: CONFIG_ANDROID_BINDERFS is not enabled in your kernel" && export FATAL=1
+    [ -n "$NO_PSI" ] && color_echo $RED "FATAL: CONFIG_PSI is not enabled in your kernel" && export FATAL=1
     [ -n "$FATAL" ] && color_echo $RED "FATAL: At least one of those mandatory kernel features are not met. You must find another kernel built with those features or build a customized kernel by yourself." && exit 1 || color_echo $GREEN "Mandatory features are met."
-    [ -n "$VA_MISSING" ] && color_echo $PURPLE "WARN: CONFIG_ARM64_VA_BITS is not 39. Some apps may crash. "
-    [ -z "$MALI_CSF_FW_MISSING" ] || color_echo $RED "ERROR: Mali firmware missing. Please place firmware_g610/mali_csffw.bin under /lib/firmware."
-    [ -z "$MALI_CSF_FW_GIT_SHA_MISMATCH" ] || color_echo $RED "ERROR: Mali firmware version mismatch. Please place firmware_g610/mali_csffw.bin under /lib/firmware."
+    [ -n "$VA_MISMATCH" ] && color_echo $PURPLE "WARN: CONFIG_ARM64_VA_BITS is not 39. Some apps may crash. "
+    [ -z "$MALI_CSF_FW_MISSING" ] || color_echo $RED "WARN: Mali firmware missing. You should find mali_csffw.bin suitable for your kernel and place it under /lib/firmware."
 }
 
 main(){
@@ -169,5 +159,3 @@ main(){
 }
 
 main "$@"
-
-exit 0
